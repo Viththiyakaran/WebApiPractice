7916e421676d45a4b567b8da6fb718c3a741b403
Viththiyakaran <battikaran6@gmail.com>
2022-05-19 23:14:25 +0530

    Create the IConfiguration

---
+++ RetailWebApi/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace RetailWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration config)
+        {
+            _configuration = config;
+        }
+    }
+}
