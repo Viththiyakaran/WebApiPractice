8334d33adec052d563f3bf5a893fc38168cf4ded
sharakenu <ahanakesu@gmail.com>
2022-05-19 22:44:19 +0530

    Enable Cors

--- RetailWebApi/Startup.cs
+++ RetailWebApi/Startup.cs
@@ -26,7 +26,11 @@ namespace RetailWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            //Enable CORS
+            services.AddCors(c => {
+                c.AddPolicy("AllowOrgin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()) ;
+            });
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -37,6 +41,11 @@ namespace RetailWebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            //Enable CORS
+            app.UseCors(options=> options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
