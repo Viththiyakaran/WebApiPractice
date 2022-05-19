640f89d2bb2d68b30aeb0f9593ad5a56c125f63a
Viththiyakaran <battikaran6@gmail.com>
2022-05-20 00:54:16 +0530

    CRUD working fine

--- RetailWebApi/Controllers/DepartmentController.cs
+++ RetailWebApi/Controllers/DepartmentController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using RetailWebApi.Model;
 
 namespace RetailWebApi.Controllers
 {
@@ -24,7 +25,7 @@ namespace RetailWebApi.Controllers
         [HttpGet]
         public JsonResult GetAllDepartment()
         {
-            string que = @"select DepartmentID from dbo.tbldepartment";
+            string que = @"select DepartmentID,Department,IsVat,VatPer from dbo.tbldepartment";
 
             DataTable dt = new DataTable();
 
@@ -42,6 +43,108 @@ namespace RetailWebApi.Controllers
                 dt.Load(dr);
 
                
+                dr.Close();
+                con.Close();
+            }
+
+            return new JsonResult(dt);
+        }
+
+
+        [HttpPost]
+        public JsonResult CreateDepartment(Department department)
+        {
+            string que = @"insert into dbo.tbldepartment(DepartmentID,Department,IsVat,VatPer) values
+                                        (@DepartmentID,@DepartmentName,@IsVat,@VatPer)";
+
+            DataTable dt = new DataTable();
+
+            string ConnectionString = _configuration.GetConnectionString("AppCon");
+
+            SqlDataReader dr;
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+
+
+                SqlCommand cmd = new SqlCommand(que, con);
+                cmd.Parameters.AddWithValue("@DepartmentID", department.DepartmentID);
+                cmd.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                cmd.Parameters.AddWithValue("@IsVat", department.IsVat);
+                cmd.Parameters.AddWithValue("@VatPer", department.VatPer);
+
+                dr = cmd.ExecuteReader();
+                dt.Load(dr);
+
+
+                dr.Close();
+                con.Close();
+            }
+
+            return new JsonResult(dt);
+        }
+
+
+        [HttpPut]
+        public JsonResult UpdateDepartment(Department department)
+        {
+            string que = @"update dbo.tbldepartment set Department =@DepartmentName, IsVat = @IsVat,  VatPer = @VatPer
+                                        where DepartmentID = @DepartmentID";
+
+            DataTable dt = new DataTable();
+
+            string ConnectionString = _configuration.GetConnectionString("AppCon");
+
+            SqlDataReader dr;
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+
+
+                SqlCommand cmd = new SqlCommand(que, con);
+                cmd.Parameters.AddWithValue("@DepartmentID", department.DepartmentID);
+                cmd.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                cmd.Parameters.AddWithValue("@IsVat", department.IsVat);
+                cmd.Parameters.AddWithValue("@VatPer", department.VatPer);
+
+                dr = cmd.ExecuteReader();
+                dt.Load(dr);
+
+
+                dr.Close();
+                con.Close();
+            }
+
+            return new JsonResult(dt);
+        }
+
+
+        [HttpDelete]
+        public JsonResult DeleteDepartment(int id)
+        {
+            string que = @"delete from dbo.tbldepartment where DepartmentID = @DepartmentID";
+
+            DataTable dt = new DataTable();
+
+            string ConnectionString = _configuration.GetConnectionString("AppCon");
+
+            SqlDataReader dr;
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+
+
+                SqlCommand cmd = new SqlCommand(que, con);
+                cmd.Parameters.AddWithValue("@DepartmentID", id);
+                
+
+                dr = cmd.ExecuteReader();
+                dt.Load(dr);
+
+
                 dr.Close();
                 con.Close();
             }
