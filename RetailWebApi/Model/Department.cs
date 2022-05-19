8a8fc02c6c2a2648bd127bb93794f25700d344ed
Viththiyakaran <battikaran6@gmail.com>
2022-05-19 23:05:27 +0530

    Create Model - Department

---
+++ RetailWebApi/Model/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetailWebApi.Model
+{
+    public class Department
+    {
+        public int DepartmentID { get; set; }
+        
+        public string DepartmentName { get; set; }
+
+        public int IsVat { get; set; }
+
+        public int VatPer { get; set; }
+        
+    }
+}
